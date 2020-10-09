03632f8d6ea2bf3eefb62c9e5206609c4550ba8d
John Silver <johncarlosilver@gmail.com>
2020-10-08 21:46:03 -0300

    printando request querys

--- src/index.js
+++ src/index.js
@@ -3,36 +3,23 @@ import { ApolloServer } from 'apollo-server'
 import { typeDefs } from './typeDefs'
 import { resolvers } from './resolvers'
 
-const myPlugin = {
-
-  // Fires whenever a GraphQL request is received from a client.
-  requestDidStart(requestContext) {
-    console.log (requestContext.request.query.name)
-
-    return {
-
-      // Fires whenever Apollo Server will parse a GraphQL
-      // request to create its associated document AST.
-      parsingDidStart(requestContext) {
-        console.log('Parsing started!');
-      },
-
-      // Fires whenever Apollo Server will validate a
-      // request's document AST against your GraphQL schema.
-      validationDidStart(requestContext) {
-        console.log('Validation started!');
-      }
-
-    }
-  },
-};
 
 const server = new ApolloServer({ 
   typeDefs, 
-  resolvers, 
-  plugins:[
-    myPlugin
-  ] 
+  resolvers,
+  formatError: (err) => {
+    console.log('GRAPHQL_ERROR: ', { message: err.message, locations: err.locations, path: err.path, extensions: { code: err.extensions.code }, stacktrace: err.extensions.exception.stacktrace })
+
+    return { message: err.message, locations: err.locations, path: err.path, extensions: { code: err.extensions.code } }
+  },
+  context: ({req}) => {
+    const afterSplit = req.body.query.split(' ')
+
+    if (!afterSplit.includes('query')){
+      console.log ("##  REQUEST  ##\nquery: ", req.body.query)
+    }
+
+  }
 });
 
 server.listen().then(({ url }) => {
